15e6ee86b97da884709e5410f5fde887386249b2
Vukasin Grebic <vgrebic@gmail.com>
2023-08-10 16:10:01 +0200

    Init commit

---
+++ utils/data-helpers.ts
@@ -0,0 +1,9 @@
+const crypto = require("crypto")
+
+export async function getRandomNummber() {
+    return Math.floor(Math.random() * 10000 + 1)
+}
+
+export async function getRandomString() {
+    return crypto.randomBytes(20).toString("hex")
+}
